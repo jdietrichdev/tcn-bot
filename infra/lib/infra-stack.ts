d7adb44abc08409b50887c458651b41c7b7bef29
Josh Dietrich <jdietrichdev@gmail.com>
2024-06-30 21:38:58 -0500

    Initial setup for infra

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,24 @@
+import { Construct } from 'constructs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { AccessLogFormat, LogGroupLogDestination, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
+
+export class InfraStack extends Stack {
+  private api: RestApi;
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const accessLogs = new LogGroup(scope, 'access-logs', {
+      logGroupName: 'BotAccessLogs',
+    })
+
+    this.api = new RestApi(scope, 'discord-bot-api', {
+      restApiName: 'DiscordBotApi',
+      deployOptions: {
+        stageName: 'prod',
+        accessLogDestination: new LogGroupLogDestination(accessLogs),
+        accessLogFormat: AccessLogFormat.jsonWithStandardFields()
+      }
+    });
+  }
+}
