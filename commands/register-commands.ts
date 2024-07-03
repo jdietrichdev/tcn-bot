61e9622786839e0c22f477d748619788505bb1f1
Josh Dietrich <jdietrichdev@gmail.com>
2024-07-02 20:43:13 -0500

    Debug registering commands

--- commands/register-commands.ts
+++ commands/register-commands.ts
@@ -2,10 +2,15 @@ import axios from 'axios';
 import { commands } from './commands';
 
 const url = `https://discord.com/api/v10/applications/${process.env.APPLICATION_ID}/commands`;
+console.log(url);
 const headers = {
     Authorization: `Bot ${process.env.BOT_TOKEN}`,
     'Content-Type': 'application/json'
 };
+console.log(process.env.BOT_TOKEN);
 axios.put(url, JSON.stringify(commands), { headers })
     .then(() => console.log('Commands updated'))
-    .catch(e => console.log('Failed to update commands', e));
+    .catch(e => {
+        console.log('Failed to update commands', e);
+        throw e;
+    });
