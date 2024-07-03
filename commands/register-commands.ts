ddfddb61aee9e2053e61f65ad91fa0189bad19c1
Josh Dietrich <jdietrichdev@gmail.com>
2024-07-02 20:50:53 -0500

    Fixing register commands workflow

--- commands/register-commands.ts
+++ commands/register-commands.ts
@@ -2,12 +2,10 @@ import axios from 'axios';
 import { commands } from './commands';
 
 const url = `https://discord.com/api/v10/applications/${process.env.APPLICATION_ID}/commands`;
-console.log(url);
 const headers = {
     Authorization: `Bot ${process.env.BOT_TOKEN}`,
     'Content-Type': 'application/json'
 };
-console.log(process.env.BOT_TOKEN);
 axios.put(url, JSON.stringify(commands), { headers })
     .then(() => console.log('Commands updated'))
     .catch(e => {
