20e54114abe407c1036007181c4da195926942cb
Josh Dietrich <jdietrichdev@gmail.com>
2024-08-30 08:08:35 -0500

    Encode COC API URL

--- bot/src/adapters/coc-api-adapter.ts
+++ bot/src/adapters/coc-api-adapter.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://cocproxy.royaleapi.dev/v1";
 
 export const verify = async (playerTag: string, apiToken: string) => {
-  const url = `${BASE_URL}/players/${playerTag}/verify`;
+  const url = encodeURI(`${BASE_URL}/players/${playerTag}/verify`);
   const response = await axios.post(url, JSON.stringify({ token: apiToken }), {
     headers: { Authorization: `Bearer ${process.env.CLASH_API_TOKEN}` },
   });
