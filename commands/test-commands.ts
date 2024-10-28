3ec3e7f0c33977e7f1f9ae9f2c105aef7dc8caf2
Josh Dietrich <jdietrichdev@gmail.com>
2024-10-28 09:25:10 -0500

    Use array for test command registering

--- commands/test-commands.ts
+++ commands/test-commands.ts
@@ -1,8 +1,10 @@
 import { SlashCommandBuilder } from "discord.js";
 
-export const testCommands = new SlashCommandBuilder()
-  .setName("test")
-  .setDescription("Test commands")
-  .addSubcommand((subcommand) => {
-    return subcommand.setName("demo").setDescription("Demo test command");
-  });
+export const testCommands = [
+  new SlashCommandBuilder()
+    .setName("test")
+    .setDescription("Test commands")
+    .addSubcommand((subcommand) => {
+      return subcommand.setName("demo").setDescription("Demo test command");
+    }),
+];
