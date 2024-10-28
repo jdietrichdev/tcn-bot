1d796b43208fa0e96ed656e78f599aaf8e2521e1
Josh Dietrich <jdietrichdev@gmail.com>
2024-10-28 09:10:19 -0500

    Updating register commands workflow to handle testing

--- commands/register-commands.ts
+++ commands/register-commands.ts
@@ -1,15 +1,28 @@
 import axios from "axios";
 import { commands } from "./commands";
+import { testCommands } from "./test-commands";
 
-const url = `https://discord.com/api/v10/applications/${process.env.APPLICATION_ID}/commands`;
+const BASE_URL = `https://discord.com/api/v10/applications/${process.env.APPLICATION_ID}`;
 const headers = {
   Authorization: `Bot ${process.env.BOT_TOKEN}`,
   "Content-Type": "application/json",
 };
 axios
-  .put(url, JSON.stringify(commands), { headers })
+  .put(`${BASE_URL}/commands`, JSON.stringify(commands), { headers })
   .then(() => console.log("Commands updated"))
   .catch((e) => {
     console.log("Failed to update commands", e);
+    throw e;
+  });
+
+axios
+  .put(
+    `${BASE_URL}/guilds/${process.env.TEST_GUILD_ID}/commands`,
+    JSON.stringify(testCommands),
+    { headers }
+  )
+  .then(() => console.log("Test commands updated"))
+  .catch((e) => {
+    console.log("Failed to update test commands", e);
     throw e;
   });
---
+++ commands/test-commands.ts
@@ -0,0 +1,8 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export const testCommands = new SlashCommandBuilder()
+  .setName("test")
+  .setDescription("Test commands")
+  .addSubcommand((subcommand) => {
+    return subcommand.setName("demo").setDescription("Demo test command");
+  });
