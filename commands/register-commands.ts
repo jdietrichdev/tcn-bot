4c2f3a3c66449368eefbf8e3d4ddd2ca922edd6e
Josh Dietrich <jdietrichdev@gmail.com>
2025-05-07 09:28:10 -0500

    Updating config and deploying TCN commands

--- commands/register-commands.ts
+++ commands/register-commands.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { testCommands } from './test-commands';
+import { testCommands } from "./test-commands";
 // import { tcnCommands } from './tcn-commands';
 
 const BASE_URL = `https://discord.com/api/v10/applications/${process.env.APPLICATION_ID}`;
@@ -19,17 +19,17 @@ axios
     console.log("Failed to update test commands", e);
     throw e;
   });
-console.log('Successfully update commands for test server');
+console.log("Successfully update commands for test server");
 
-// axios
-//   .put(
-//     `${BASE_URL}/guilds/${process.env.TCN_GUILD_ID}/commands`,
-//     JSON.stringify(tcnCommands),
-//     { headers }
-//   )
-//   .then(() => console.log("Test commands updated"))
-//   .catch((e) => {
-//     console.log("Failed to update test commands", e);
-//     throw e;
-//   });
-// console.log('Successfully updated commands for TCN server');
+axios
+  .put(
+    `${BASE_URL}/guilds/${process.env.TCN_GUILD_ID}/commands`,
+    JSON.stringify(tcnCommands),
+    { headers }
+  )
+  .then(() => console.log("Test commands updated"))
+  .catch((e) => {
+    console.log("Failed to update test commands", e);
+    throw e;
+  });
+console.log("Successfully updated commands for TCN server");
