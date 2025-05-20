3e84560275fa8df2245efe7016535ba46c66655d
jdietrichdev <53873638+jdietrichdev@users.noreply.github.com>
2025-05-19 21:39:45 -0500

    Initial scoring compilation

--- bot/src/command-handlers/recruiterScore.ts
+++ bot/src/command-handlers/recruiterScore.ts
@@ -10,6 +10,7 @@ export const handleRecruiterScore = async (
 ) => {
   try {
     const config = getConfig(interaction.guild_id!);
+    const scoreMap = new Map<string, Record<string, number>>();
     const messages = await getChannelMessages(
       config.RECRUITMENT_OPP_CHANNEL,
       new Date(new Date().getTime() - 1 * 24 * 60 * 60 * 1000)
@@ -19,13 +20,20 @@ export const handleRecruiterScore = async (
       if (message.type === MessageType.ChatInputCommand &&
           message.embeds[0].title != "You're on Cooldown"
       ) {
-        console.log("Bump: " + JSON.stringify(message));
-        console.log("User: " + message.interaction_metadata?.user.username);
+        const user = message.interaction_metadata!.user.username;
+        const stats = scoreMap.get(user) || { bump: 0, forward: 0, reachOut: 0 };
+        stats.bump++;
+        scoreMap.set(user, stats);
+        console.log("Bump user: " + message.interaction_metadata?.user.username);
       } else if (message.type === MessageType.Default && message.message_reference) {
-        console.log("Forward: " + JSON.stringify(message));
-        console.log("User: " + message.author.username);
+        const user = message.author.username;
+        const stats = scoreMap.get(user) || { bump: 0, forward: 0, reachOut: 0 };
+        stats.forward++;
+        scoreMap.set(user, stats);
+        console.log("Forward user: " + message.author.username);
         console.log("Reactions: " + JSON.stringify(message.reactions));
       }
+      console.log(JSON.stringify(Object.fromEntries(scoreMap)));
     }
   } catch (err) {
     console.error(`Failed to generate recruitment score: ${err}`);
