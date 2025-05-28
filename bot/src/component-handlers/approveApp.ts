8f4f5d37a1b900189e72c5c60164682c72457b33
Josh Dietrich <jdietrichdev@gmail.com>
2025-05-28 13:36:12 -0500

    Sanitizing username for channel creation

--- bot/src/component-handlers/approveApp.ts
+++ bot/src/component-handlers/approveApp.ts
@@ -74,7 +74,7 @@ const createApplicationChannel = async (
   const username = interaction.message.embeds[0].title?.split(" ")[2];
   const channel = await createChannel(
     {
-      name: `\u{1F39F}-${username}`,
+      name: `\u{1F39F}-${username?.toLowerCase().replace(/[^a-z0-9_-]/g, "")}`,
       type: ChannelType.GuildText,
       topic: `Application channel for ${username}:${userId}`,
       parent_id: config.APPLICATION_CATEGORY,
