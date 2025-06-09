03e13c71ff321eabd6fada90298037e6903352b3
Josh Dietrich <jdietrichdev@gmail.com>
2025-06-09 15:03:59 -0500

    Adding timeout for processor

--- infra/lib/service-stack.ts
+++ infra/lib/service-stack.ts
@@ -165,6 +165,7 @@ export class ServiceStack extends Stack {
         CLASH_API_TOKEN: process.env.CLASH_API_TOKEN!,
         BOT_TOKEN: process.env.BOT_TOKEN!,
       },
+      timeout: Duration.minutes(3),
     });
 
     rosterBucket.addEventNotification(
