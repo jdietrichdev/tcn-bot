6ccca211a4b7179ac1c976fafcc70b4fe29a30d7
jdietrichdev <53873638+jdietrichdev@users.noreply.github.com>
2025-06-10 21:47:47 -0500

    Rework of sending messages

--- bot/src/command-handlers/cwlRoster.ts
+++ bot/src/command-handlers/cwlRoster.ts
@@ -1,6 +1,7 @@
 import {
   APIApplicationCommandInteractionDataStringOption,
   APIChatInputApplicationCommandInteraction,
+  RESTPostAPIWebhookWithTokenJSONBody,
 } from "discord-api-types/v10";
 import { getConfig, ServerConfig } from "../util/serverConfig";
 import { updateResponse } from "../adapters/discord-adapter";
@@ -38,8 +39,8 @@ export const handleCwlRoster = async (
 
     if (notificationType === "Announcement") {
       console.log("Sending roster announcement");
-      const announcement = buildAnnouncement(rosterData.roster, config);
-      console.log(announcement);
+      const announcementMessages = buildAnnouncement(rosterData.roster, config);
+      console.log(JSON.stringify(announcementMessages));
     } else if (notificationType === "Reminder") {
       console.log("Sending roster reminder");
     }
@@ -53,13 +54,14 @@ export const handleCwlRoster = async (
 };
 
 const buildAnnouncement = (roster: Record<string, any>[], config: ServerConfig) => {
-  let announcement = `<@&${config.CLAN_ROLE}>\nRosters have been set for the upcoming CWL season! Please take a look and feel free to reach out to leads/admins if you have questions about placement or don't see your accounts in the list.\n`;
+  const messages: RESTPostAPIWebhookWithTokenJSONBody[] = []
+  messages.push({ content: `<@&${config.CLAN_ROLE}>\nRosters have been set for the upcoming CWL season! Please take a look and feel free to reach out to leads/admins if you have questions about placement or don't see your accounts in the list.` });
   for (const clan of roster) {
-    announcement += `${clan.league}\n`;
-    announcement += `${clan.clanTag}\n`;
+    let message = `**${clan.league}**\n${clan.clanTag}\n`;
     for (const player of clan.players) {
-      announcement += `<@${player.userId}> ${player.playerName}`;
+      message += `<@${player.userId}> ${player.playerName}\n`;
     }
+    messages.push({ content: message });
   }
-  return announcement;
+  return messages;
 }
