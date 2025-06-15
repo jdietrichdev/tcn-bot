008dc5cfd8301f528debd24d3ea890e9e8ce6900
jdietrichdev <53873638+jdietrichdev@users.noreply.github.com>
2025-06-14 22:25:35 -0500

    Headers for different roster message types

--- bot/src/command-handlers/cwlRoster.ts
+++ bot/src/command-handlers/cwlRoster.ts
@@ -54,7 +54,7 @@ export const handleCwlRoster = async (
       }
       const previousMessages: string[] = [];
       const { id } = await sendMessage({
-        content: `<@&${config.CLAN_ROLE}>\nRosters have been set for the upcoming CWL season! Please take a look and feel free to reach out to leads/admins if you have questions about placement or don't see your accounts in the list.`,
+        content: `[ANNOUNCEMENT]\n<@&${config.CLAN_ROLE}>\nRosters have been set for the upcoming CWL season! Please take a look and feel free to reach out to leads/admins if you have questions about placement or don't see your accounts in the list.`,
       }, config.ANNOUNCEMENT_CHANNEL);
       previousMessages.push(id);
       await new Promise((resolve) => setTimeout(resolve, 1500));
@@ -121,7 +121,7 @@ const buildReminder = async (roster: Record<string, any>[]) => {
   const messages: RESTPostAPIWebhookWithTokenJSONBody[] = [];
   messages.push({
     content:
-      "CWL spin is coming soon! Please make sure you are in the right clan and help those that are not!",
+      "[REMINDER]\nCWL spin is coming soon! Please make sure you are in the right clan and help those that are not!",
   });
   for (const clan of roster) {
     const cwlStatus = await getCwl(`#${clan.clanTag}`);
