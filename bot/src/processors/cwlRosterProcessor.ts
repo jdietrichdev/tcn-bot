3ad0f57f7815b6bc852fa8f17c14f2bbeba02572
jdietrichdev <53873638+jdietrichdev@users.noreply.github.com>
2025-06-15 06:19:22 -0500

    Don't process folders being created in S3

--- bot/src/processors/cwlRosterProcessor.ts
+++ bot/src/processors/cwlRosterProcessor.ts
@@ -26,79 +26,83 @@ export const processCwlRoster = async (event: S3Event) => {
     for (const record of event.Records) {
       const bucket = record.s3.bucket.name;
       const key = record.s3.object.key;
+      
+      if (record.s3.object.size !== 0) {
+        const response = await s3Client.send(
+          new GetObjectCommand({
+            Bucket: bucket,
+            Key: key,
+          })
+        );
 
-      const response = await s3Client.send(
-        new GetObjectCommand({
-          Bucket: bucket,
-          Key: key,
-        })
-      );
+        const csvData = await streamCsv(response.Body as Readable);
 
-      const csvData = await streamCsv(response.Body as Readable);
+        const records: Record<string, string>[] = parse(csvData, {
+          columns: true,
+          skip_empty_lines: true,
+          trim: true,
+        });
 
-      const records: Record<string, string>[] = parse(csvData, {
-        columns: true,
-        skip_empty_lines: true,
-        trim: true,
-      });
+        const guildId = key.split("/")[0];
+        const guildMembers = await getServerMembers(guildId);
 
-      const guildId = key.split("/")[0];
-      const guildMembers = await getServerMembers(guildId);
-
-      let clanTag = "";
-      let league = "";
-      const clanMap = new Map<string, ClanRoster>();
-      for (const record of records) {
-        if (
-          !Object.values(record).every((value) => value.trim() === "") &&
-          record["@"] !== "@"
-        ) {
+        let clanTag = "";
+        let league = "";
+        const clanMap = new Map<string, ClanRoster>();
+        for (const record of records) {
           if (
-            record["@"] === "" &&
-            record["Player Name"] !== "" &&
-            record["Combined Heroes"] !== ""
+            !Object.values(record).every((value) => value.trim() === "") &&
+            record["@"] !== "@"
           ) {
-            league = record["Player Name"];
-            clanTag = record["Combined Heroes"].split("=")[2];
-            if (clanTag.startsWith("%23")) clanTag = clanTag.replace("%23", "");
-            clanMap.set(clanTag, {
-              clanTag,
-              league,
-              players: [],
-            });
-          } else if (record["Player Name"] !== "" && record["Discord"] !== "") {
-            const clanRoster = clanMap.get(clanTag);
-            clanRoster?.players.push({
-              playerTag: record["Player Tag"],
-              playerName: record["Player Name"],
-              userId:
-                guildMembers.find(
-                  (member: APIGuildMember) =>
-                    member.user.username === record["Discord"]
-                )?.user.id ?? record["Discord"],
-            });
+            if (
+              record["@"] === "" &&
+              record["Player Name"] !== "" &&
+              record["Combined Heroes"] !== ""
+            ) {
+              league = record["Player Name"];
+              clanTag = record["Combined Heroes"].split("=")[2];
+              if (clanTag.startsWith("%23")) clanTag = clanTag.replace("%23", "");
+              clanMap.set(clanTag, {
+                clanTag,
+                league,
+                players: [],
+              });
+            } else if (record["Player Name"] !== "" && record["Discord"] !== "") {
+              const clanRoster = clanMap.get(clanTag);
+              clanRoster?.players.push({
+                playerTag: record["Player Tag"],
+                playerName: record["Player Name"],
+                userId:
+                  guildMembers.find(
+                    (member: APIGuildMember) =>
+                      member.user.username === record["Discord"]
+                  )?.user.id ?? record["Discord"],
+              });
+            }
           }
         }
-      }
 
-      console.log("Storing roster to table");
-      const tableResponse = await dynamoDbClient.send(
-        new UpdateCommand({
-          TableName: "BotTable",
-          Key: {
-            pk: guildId,
-            sk: `roster#${key.split("/")[1].replace(".csv", "")}`,
-          },
-          ExpressionAttributeNames: {
-            "#roster": "roster",
-          },
-          ExpressionAttributeValues: {
-            ":roster": Array.from(clanMap.values()),
-          },
-          UpdateExpression: "set #roster = :roster",
-        })
-      );
-      console.log(`Stored roster to table: ${JSON.stringify(tableResponse)}`);
+        console.log("Storing roster to table");
+        const tableResponse = await dynamoDbClient.send(
+          new UpdateCommand({
+            TableName: "BotTable",
+            Key: {
+              pk: guildId,
+              sk: `roster#${key.split("/")[1].replace(".csv", "")}`,
+            },
+            ExpressionAttributeNames: {
+              "#roster": "roster",
+            },
+            ExpressionAttributeValues: {
+              ":roster": Array.from(clanMap.values()),
+            },
+            UpdateExpression: "set #roster = :roster",
+          })
+        );
+        console.log(`Stored roster to table: ${JSON.stringify(tableResponse)}`);
+      } else {
+        console.log("Folder created, no processing required");
+      }
     }
   } catch (err) {
     console.log(`Failed processing CWL roster: ${err}`);
