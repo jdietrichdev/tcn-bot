49965fb0f4928cd1bb25a8ae229dc640a8103ec3
Josh Dietrich <jdietrichdev@gmail.com>
2025-06-23 14:32:03 -0500

    Creating standalone Next UI for bot application

---
+++ infra/lib/ui-stack.ts
@@ -0,0 +1,22 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+import { Nextjs } from "cdk-nextjs-standalone";
+
+interface UIStackProps extends StackProps {
+  transcriptBucket: Bucket;
+}
+
+export class UIStack extends Stack {
+  readonly nextjs: Nextjs;
+
+  constructor(scope: Construct, id: string, props: UIStackProps) {
+    super(scope, id, props);
+
+    this.nextjs = new Nextjs(this, "bot-ui", {
+      nextjsPath: "./",
+    });
+
+    props.transcriptBucket.grantRead(this.nextjs.serverFunction.lambdaFunction);
+  }
+}
