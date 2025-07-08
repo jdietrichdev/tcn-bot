b74a810564b077699f8b6bfa0d2fb4e905133af8
Josh Dietrich <jdietrichdev@gmail.com>
2025-07-08 11:05:39 -0500

    Send new message to channel
    Debug logging for signup

--- bot/src/command-handlers/initiateCwlSignup.ts
+++ bot/src/command-handlers/initiateCwlSignup.ts
@@ -1,35 +1,49 @@
-import { APIApplicationCommandInteractionDataStringOption, APIChatInputApplicationCommandInteraction, ComponentType } from "discord-api-types/v10";
+import {
+  APIApplicationCommandInteractionDataStringOption,
+  APIChatInputApplicationCommandInteraction,
+  ComponentType,
+} from "discord-api-types/v10";
 import { getCommandOptionData } from "../util/interaction-util";
-import { updateResponse } from "../adapters/discord-adapter";
+import { sendMessage, updateResponse } from "../adapters/discord-adapter";
 import { BUTTONS } from "../component-handlers/buttons";
+import { getConfig } from "../util/serverConfig";
 
-export const handleInitiateCwlSignup = async (interaction: APIChatInputApplicationCommandInteraction) => {
-    try {
-        const signupName = getCommandOptionData<APIApplicationCommandInteractionDataStringOption>(
-            interaction,
-            'name'
-        ).value;
+export const handleInitiateCwlSignup = async (
+  interaction: APIChatInputApplicationCommandInteraction
+) => {
+  try {
+    const config = getConfig(interaction.guild_id!);
+    const signupName =
+      getCommandOptionData<APIApplicationCommandInteractionDataStringOption>(
+        interaction,
+        "name"
+      ).value;
 
-        await updateResponse(interaction.application_id, interaction.token, {
-            embeds: [{
-                title: signupName,
-                fields: [],
-                footer: {
-                    text: 'Total accounts: 0\nSignup is **open**'
-                }
-            }],
-            components: [
-                {
-                    type: ComponentType.ActionRow,
-                    components: [
-                        BUTTONS.SIGNUP_CWL,
-                        BUTTONS.CLOSE_CWL_SIGNUP,
-                    ]
-                }
-            ],
-        });
-    } catch (err) {
-        console.log(`Failed to initiate CWL signup: ${err}`);
-        throw err;
-    }
-}
+    await sendMessage(
+      {
+        embeds: [
+          {
+            title: signupName,
+            fields: [],
+            footer: {
+              text: "Total accounts: 0\nSignup is **open**",
+            },
+          },
+        ],
+        components: [
+          {
+            type: ComponentType.ActionRow,
+            components: [BUTTONS.SIGNUP_CWL, BUTTONS.CLOSE_CWL_SIGNUP],
+          },
+        ],
+      },
+      config.CWL_SIGNUP_CHANNEL
+    );
+    await updateResponse(interaction.application_id, interaction.token, {
+      content: "CWL signup has been started",
+    });
+  } catch (err) {
+    console.log(`Failed to initiate CWL signup: ${err}`);
+    throw err;
+  }
+};
--- bot/src/component-handlers/signupCwl.ts
+++ bot/src/component-handlers/signupCwl.ts
@@ -1,40 +1,56 @@
-import { APIMessageComponentInteraction, ComponentType } from "discord-api-types/v10";
+import {
+  APIMessageComponentInteraction,
+  ComponentType,
+} from "discord-api-types/v10";
 import { dynamoDbClient } from "../clients/dynamodb-client";
 import { QueryCommand } from "@aws-sdk/lib-dynamodb";
 import { getMessageSender } from "../util/interaction-util";
 import { updateResponse } from "../adapters/discord-adapter";
 
-export const signupCwl = async (interaction: APIMessageComponentInteraction) => {
-    try {
-        const actor = getMessageSender(interaction);
-        const accounts = (await dynamoDbClient.send(new QueryCommand({
-            TableName: "BotTable",
-            KeyConditionExpression: `pk = :pk AND begins_with(sk, :prefix)`,
-            ExpressionAttributeValues: {
-                ":pk": actor.id,
-                ":prefix": "player#"
-            }
-        }))).Items;
-        await updateResponse(interaction.application_id, interaction.token, {
-            content: "Select the account you would like to sign up",
-            components: [{
-                type: ComponentType.ActionRow,
-                components: [{
-                    type: ComponentType.StringSelect,
-                    custom_id: 'signupAccount',
-                    placeholder: "Select Account...",
-                    max_values: 1,
-                    options: accounts ? accounts.map((account) => {
-                        return {
-                            label: `${account.tag} ${account.username}`,
-                            value: account.username
-                        }
-                    }) : []
-                }]
-            }]
+export const signupCwl = async (
+  interaction: APIMessageComponentInteraction
+) => {
+  try {
+    const actor = getMessageSender(interaction);
+    const accounts = (
+      await dynamoDbClient.send(
+        new QueryCommand({
+          TableName: "BotTable",
+          KeyConditionExpression: `pk = :pk AND begins_with(sk, :prefix)`,
+          ExpressionAttributeValues: {
+            ":pk": actor.id,
+            ":prefix": "player#",
+          },
         })
-    } catch (err) {
-        console.log(`Failed to handle signup: ${err}`);
-        throw err;
-    }
-}
+      )
+    ).Items;
+    console.log(JSON.stringify(accounts));
+    await updateResponse(interaction.application_id, interaction.token, {
+      content: "Select the account you would like to sign up",
+      components: [
+        {
+          type: ComponentType.ActionRow,
+          components: [
+            {
+              type: ComponentType.StringSelect,
+              custom_id: "signupAccount",
+              placeholder: "Select Account...",
+              max_values: 1,
+              options: accounts
+                ? accounts.map((account) => {
+                    return {
+                      label: `${account.tag} ${account.username}`,
+                      value: account.tag,
+                    };
+                  })
+                : [],
+            },
+          ],
+        },
+      ],
+    });
+  } catch (err) {
+    console.log(`Failed to handle signup: ${err}`);
+    throw err;
+  }
+};
