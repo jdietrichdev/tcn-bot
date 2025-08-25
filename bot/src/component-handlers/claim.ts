71051e423a982d6ddbfb550dfe26bcfbbeda3b6d
jdietrichdev <53873638+jdietrichdev@users.noreply.github.com>
2025-08-24 22:25:45 -0500

    Open claim ticket on button press

---
+++ bot/src/component-handlers/claim.ts
@@ -0,0 +1,81 @@
+import { APIMessageComponentInteraction, ChannelType, OverwriteType, PermissionFlagsBits } from "discord-api-types/v10";
+import { ServerConfig } from "../util/serverConfig";
+import { dynamoDbClient } from "../clients/dynamodb-client";
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
+import { createChannel, sendMessage, updateMessage, updateResponse } from "../adapters/discord-adapter";
+
+export const claimEvent = async (interaction: APIMessageComponentInteraction, config: ServerConfig) => {
+    try {
+        const [, guildId, eventId] = interaction.data.custom_id.split("_");
+        const eventData = (await dynamoDbClient.send(new GetCommand({
+            TableName: "BotTable",
+            Key: {
+                pk: guildId,
+                sk: `event#${eventId}`
+            }
+        }))).Item!;
+
+        const channel = await createChannel({
+            name: `🏆-${interaction.user!.username}`,
+            topic: `Claim prize from ${eventData.name}`,
+            type: ChannelType.GuildText,
+            parent_id: config.EVENTS_CATEGORY,
+            permission_overwrites: [
+                {
+                    id: interaction.guild_id!,
+                    type: OverwriteType.Role,
+                    allow: "0",
+                    deny: PermissionFlagsBits.ViewChannel.toString(),
+                },
+                {
+                    id: config.BOT_ID,
+                    type: OverwriteType.Member,
+                    allow: (
+                        PermissionFlagsBits.ViewChannel |
+                        PermissionFlagsBits.AddReactions |
+                        PermissionFlagsBits.SendMessages
+                    ).toString(),
+                    deny: "0",
+                },
+                {
+                    id: interaction.user!.id,
+                    type: OverwriteType.Member,
+                    allow: (
+                        PermissionFlagsBits.ViewChannel |
+                        PermissionFlagsBits.AddReactions |
+                        PermissionFlagsBits.SendMessages
+                    ).toString(),
+                    deny: "0",
+                },
+                {
+                    id: eventData.sponsor,
+                    type: OverwriteType.Member,
+                    allow: (
+                        PermissionFlagsBits.ViewChannel |
+                        PermissionFlagsBits.AddReactions |
+                        PermissionFlagsBits.SendMessages
+                    ).toString(),
+                    deny: "0",
+                }
+            ]
+        }, guildId);
+
+        await sendMessage({
+            content: `Congrats again on winning a prize in our event <@${interaction.user!.id}>! Please coordinate with <@${eventData.sponsor}> to claim your prize.`
+        }, channel.id);
+
+        await updateMessage(interaction.channel.id, interaction.message.id, {
+            components: []
+        });
+
+        await updateResponse(interaction.application_id, interaction.token, {
+            content: "Your claim process has begun!"
+        });
+    } catch (err) {
+        console.error(`Failed to claim prize: ${err}`);
+        await updateResponse(interaction.application_id, interaction.token, {
+            content:
+                "There was a failure claiming your prize, please try again or contact admin",
+        });
+    }
+}
--- bot/src/component-handlers/index.ts
+++ bot/src/component-handlers/index.ts
@@ -12,47 +12,38 @@ import { confirmDelete } from "./confirmDelete";
 import { rejectDelete } from "./rejectDelete";
 import { signupCwl } from "./signupCwl";
 import { exportCwlQuestions } from "./exportCwlQuestions";
+import { claimEvent } from "./claim";
 
 export const handleComponent = async (
   interaction: APIMessageComponentInteraction
 ) => {
   const config = getConfig(interaction.guild_id!);
-  switch (interaction.data.custom_id) {
-    case "approveApp":
-      await approveApp(interaction, config);
-      break;
-    case "messageRecruit":
-      await messageRecruit(interaction);
-      break;
-    case "closeRecruit":
-      await closeRecruit(interaction);
-      break;
-    case "closeTicket":
-      await closeTicket(interaction, config);
-      break;
-    case "deleteTicket":
-      await deleteTicket(interaction, config);
-      break;
-    case "reopenTicket":
-      await reopenTicket(interaction, config);
-      break;
-    case "grantRoles":
-      await grantRoles(interaction, config);
-      break;
-    case "removeRoles":
-      await removeRoles(interaction, config);
-      break;
-    case "confirmDelete":
-      await confirmDelete(interaction);
-      break;
-    case "rejectDelete":
-      await rejectDelete(interaction);
-      break;
-    case "signupCwl":
-      await signupCwl(interaction);
-      break;
-    case "exportCwlQuestions":
-      await exportCwlQuestions(interaction);
-      break;
+  const customId = interaction.data.custom_id;
+  if (customId === 'approveApp') {
+    await approveApp(interaction, config);
+  } else if (customId === 'messageRecruit') {
+    await messageRecruit(interaction);
+  } else if (customId === 'closeRecruit') {
+    await closeRecruit(interaction);
+  } else if (customId === 'closeTicket') {
+    await closeTicket(interaction, config);
+  } else if (customId === "deleteTicket") {
+    await deleteTicket(interaction, config);
+  } else if (customId === 'reopenTicket') {
+    await reopenTicket(interaction, config);
+  } else if (customId === 'grantRoles') {
+    await grantRoles(interaction, config);
+  } else if (customId === 'removeRoles') {
+    await removeRoles(interaction, config);
+  } else if (customId === 'confirmDelete') {
+    await confirmDelete(interaction);
+  } else if (customId === 'rejectDelete') {
+    await rejectDelete(interaction);
+  } else if (customId === 'signupCwl') {
+    await signupCwl(interaction);
+  } else if (customId === 'exportCwlQuestions') {
+    await exportCwlQuestions(interaction);
+  } else if (customId.startsWith("claim")) {
+    await claimEvent(interaction, config);
   }
 };
