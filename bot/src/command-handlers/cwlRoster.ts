8abb74709cde6e25da2894714d6a845aee7c1c90
Josh Dietrich <jdietrichdev@gmail.com>
2025-09-30 06:22:52 -0500

    Bold correctly

--- bot/src/command-handlers/cwlRoster.ts
+++ bot/src/command-handlers/cwlRoster.ts
@@ -225,7 +225,7 @@ const buildAnnouncement = async (roster: Record<string, any>[]) => {
   }
   messages.push({
     content:
-      "Believe you should be in a higher/lower CWL? Please reach out to a lead/admin to schedule FCs ASAP. **Changes can be made as long as you don't ask last second!",
+      "Believe you should be in a higher/lower CWL? Please reach out to a lead/admin to schedule FCs ASAP. **Changes can be made as long as you don't ask last second!**",
   });
   messages.push({
     content: `*Last updated: <t:${createDiscordTimestamp(
