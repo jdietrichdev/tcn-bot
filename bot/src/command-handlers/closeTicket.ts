3d56e98775efae2192fd79e34d8a1c9c8badee23
Josh Dietrich <jdietrichdev@gmail.com>
2025-10-01 12:56:18 -0500

    Updating determination of ticket channel

--- bot/src/command-handlers/closeTicket.ts
+++ bot/src/command-handlers/closeTicket.ts
@@ -1,5 +1,5 @@
 import {
-    APIApplicationCommandInteraction,
+  APIApplicationCommandInteraction,
   APITextChannel,
   OverwriteType,
 } from "discord-api-types/v10";
@@ -19,42 +19,46 @@ export const closeTicket = async (
   try {
     const config = getConfig(interaction.guild_id!);
     if (isTicketChannel(interaction.channel.name!)) {
-        if (
+      if (
         await isActorRecruiter(
-            interaction.guild_id!,
-            interaction.member!.user.id,
-            config
+          interaction.guild_id!,
+          interaction.member!.user.id,
+          config
         )
-        ) {
+      ) {
         const channelId = interaction.channel.id;
         const userId = (interaction.channel as APITextChannel).topic!.split(
-            ":"
+          ":"
         )[1];
         await moveChannel(channelId, config.ARCHIVE_CATEGORY);
         await updateChannelPermissions(channelId, userId, {
-            type: OverwriteType.Member,
-            allow: "0",
-            deny: "0",
+          type: OverwriteType.Member,
+          allow: "0",
+          deny: "0",
         });
         await sendMessage(
-            {
+          {
             content: `${interaction.member?.user.username} has closed the ticket`,
-            },
-            channelId
+          },
+          channelId
         );
-        } else {
-            await sendMessage(
-                {
-                content: `You do not have permission to close this ticket <@${interaction.member?.user.id}>`,
-                },
-                interaction.channel.id
-            );
-        }
-    } else {
+      } else {
         await sendMessage(
-            { content: `This is not an application channel <@${interaction.member!.user.id}>`}, 
-            interaction.channel.id
+          {
+            content: `You do not have permission to close this ticket <@${interaction.member?.user.id}>`,
+          },
+          interaction.channel.id
         );
+      }
+    } else {
+      await sendMessage(
+        {
+          content: `This is not an application channel <@${
+            interaction.member!.user.id
+          }>`,
+        },
+        interaction.channel.id
+      );
     }
     await deleteResponse(interaction.application_id, interaction.token);
   } catch (err) {
@@ -67,5 +71,5 @@ export const closeTicket = async (
 };
 
 const isTicketChannel = (channelName: string) => {
-    return channelName.includes('\u{1F39F}');
-}
+  return channelName.includes("\u{1F39F}") || channelName.includes("\u{2705}");
+};
