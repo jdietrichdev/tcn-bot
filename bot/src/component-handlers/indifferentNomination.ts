b37da377f0587f7999c11619661cae4c4994e048
Josh Dietrich <jdietrichdev@gmail.com>
2025-10-06 14:56:21 -0500

    Update message on not sure

--- bot/src/component-handlers/indifferentNomination.ts
+++ bot/src/component-handlers/indifferentNomination.ts
@@ -1,15 +1,17 @@
 import { APIMessageComponentInteraction } from "discord-api-types/v10";
-import { updateResponse } from "../adapters/discord-adapter";
+import { updateMessage, updateResponse } from "../adapters/discord-adapter";
 import { dynamoDbClient } from "../clients/dynamodb-client";
 import { GetCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { Proposal } from "../util/interfaces";
 import { VoteType } from "../util/enums";
-import { addVote } from "../util/nomination-util";
+import { addVote, tallyVotes } from "../util/nomination-util";
+import { getConfig } from "../util/serverConfig";
 
 export const indifferentNomination = async (
   interaction: APIMessageComponentInteraction
 ) => {
   try {
+    const config = getConfig(interaction.guild_id!);
     const message = interaction.message.id;
 
     const proposalData = (
@@ -30,7 +32,24 @@ export const indifferentNomination = async (
 
     addVote(interaction, proposal.votes, VoteType.NOT_SURE);
 
-    //Do not update embed
+    const updatedEmbed = interaction.message.embeds[0];
+    const [yes, no] = tallyVotes(proposal.votes);
+
+    updatedEmbed.fields = [
+      ...((yes &&
+        no && [
+          {
+            name: "Current Status",
+            value: `Yes: ${yes}, No: ${no}`,
+          },
+        ]) ||
+        []),
+    ];
+    console.log(updatedEmbed);
+
+    await updateMessage(config.RANK_PROPOSAL_CHANNEL, message, {
+      embeds: [updatedEmbed],
+    });
 
     await dynamoDbClient.send(
       new PutCommand({
