6a869520abb21803c8f366c8f89b7aee7cd6c59d
jdietrichdev <53873638+jdietrichdev@users.noreply.github.com>
2025-10-13 21:05:03 -0500

    Compiling roster data

--- bot/src/command-handlers/announceRoster.ts
+++ bot/src/command-handlers/announceRoster.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { parse } from "csv-parse/sync";
-import { APIChatInputApplicationCommandInteraction } from "discord-api-types/v10";
-import { updateResponse } from "../adapters/discord-adapter";
+import { APIChatInputApplicationCommandInteraction, APIGuildMember } from "discord-api-types/v10";
+import { getServerMembers, updateResponse } from "../adapters/discord-adapter";
+import { getClan } from "../adapters/coc-api-adapter";
 
 const ROSTER_URL =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vRckzbRnsega-kND3dWkpaeMe78An7gD6Z3YM-vkaxTyXf1KMXDIgNB917_sJ5zyhNT7LKwK6fWstnJ/pub?gid=914552917&single=true&output=csv";
@@ -16,7 +17,37 @@ export const handleAnnounceRoster = async (
       skip_empty_lines: true,
     });
 
-    console.log(JSON.stringify(rosterData));
+    const guildMembers = await getServerMembers(interaction.guild_id!);
+    const roster = [];
+    let clanRoster: Record<string, any> = {};
+    let clanData: Record<string, any>;
+
+    for (const row of rosterData) {
+        if (row["Tag"].startsWith("https://link.clashofclans.com/en/?action=OpenClanProfile&tag=")) {
+            if (clanRoster) {
+                roster.push(clanRoster);
+            }
+            const tag = row["Tag"].split("=")[2];
+            clanData = await getClan(tag);
+            clanRoster = {
+                tag,
+                name: clanData.name,
+                members: []
+            }
+        } else if (row["Tag"].startsWith("#")) {
+            const userId = guildMembers.find(
+                (member: APIGuildMember) =>
+                    member.user.username === row["Discord"]
+                )?.user.id ?? row["Discord"];
+            clanRoster.members.push({
+                playerTag: row["Tag"],
+                playerName: row["Account"],
+                userId: userId,
+            });
+        }
+    }
+
+    console.log(JSON.stringify(roster));
     await updateResponse(interaction.application_id, interaction.token, {
       content: "Roster has been announced",
     });
