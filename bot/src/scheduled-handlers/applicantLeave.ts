68087a2b6660e9e1bd740b8b66017e8bf100d469
Josh Dietrich <jdietrichdev@gmail.com>
2025-10-20 14:09:32 -0500

    Adding log when message is sent

--- bot/src/scheduled-handlers/applicantLeave.ts
+++ bot/src/scheduled-handlers/applicantLeave.ts
@@ -5,46 +5,59 @@ import { DiscordError } from "../util/errors";
 import { ComponentType } from "discord-api-types/v10";
 import { BUTTONS } from "../component-handlers/buttons";
 
-export const handleApplicantLeave = async (eventDetail: Record<string, string>) => {
-    try {
-        const { guildId } = eventDetail;
-        const ticketData = (await dynamoDbClient.send(
-            new GetCommand({
-                TableName: "BotTable",
-                Key: {
-                    pk: guildId,
-                    sk: 'tickets'
-                }
-            })
-        )).Item!;
-        const tickets = ticketData.tickets;
+export const handleApplicantLeave = async (
+  eventDetail: Record<string, string>
+) => {
+  try {
+    const { guildId } = eventDetail;
+    const ticketData = (
+      await dynamoDbClient.send(
+        new GetCommand({
+          TableName: "BotTable",
+          Key: {
+            pk: guildId,
+            sk: "tickets",
+          },
+        })
+      )
+    ).Item!;
+    const tickets = ticketData.tickets;
 
-        for (const ticket of tickets) {
-            try {
-                if (!ticket.applicantLeft) {
-                    await getServerUser(guildId, ticket.userId);
-                }
-            } catch (err) {
-                if ((err as DiscordError).statusCode === 404) {
-                    await sendMessage({
-                        content: `<@${ticket.userId}> has left the server, what would you like to do with this ticket?`,
-                        components: [{
-                            type: ComponentType.ActionRow,
-                            components: [BUTTONS.CLOSE_TICKET, BUTTONS.DELETE_TICKET]
-                        }]
-                    }, ticket.ticketChannel);
-                    ticket.applicantLeft = true;
-                    await dynamoDbClient.send(
-                        new PutCommand({
-                            TableName: "BotTable",
-                            Item: ticketData
-                        })
-                    );
-                }
-            }
+    for (const ticket of tickets) {
+      try {
+        if (!ticket.applicantLeft) {
+          await getServerUser(guildId, ticket.userId);
         }
-    } catch (err) {
-        console.error("Failed to handle checking for applicants who left server", err);
-        throw err;
+      } catch (err) {
+        if ((err as DiscordError).statusCode === 404) {
+          console.log(`Sending applicant left message for ${ticket.userId}`);
+          await sendMessage(
+            {
+              content: `<@${ticket.userId}> has left the server, what would you like to do with this ticket?`,
+              components: [
+                {
+                  type: ComponentType.ActionRow,
+                  components: [BUTTONS.CLOSE_TICKET, BUTTONS.DELETE_TICKET],
+                },
+              ],
+            },
+            ticket.ticketChannel
+          );
+          ticket.applicantLeft = true;
+          await dynamoDbClient.send(
+            new PutCommand({
+              TableName: "BotTable",
+              Item: ticketData,
+            })
+          );
+        }
+      }
     }
-}
+  } catch (err) {
+    console.error(
+      "Failed to handle checking for applicants who left server",
+      err
+    );
+    throw err;
+  }
+};
