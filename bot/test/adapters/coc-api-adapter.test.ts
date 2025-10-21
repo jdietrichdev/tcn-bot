81025e0dbed51442ec5a9b2368a2046b93ce6fe3
jdietrichdev <53873638+jdietrichdev@users.noreply.github.com>
2025-10-20 22:24:01 -0500

    COC Adapter and authorizer testing

--- bot/test/adapters/coc-api-adapter.test.ts
+++ bot/test/adapters/coc-api-adapter.test.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { verify } from "../../src/adapters/coc-api-adapter";
+import { getClan, getCwl, getPlayer, verify } from "../../src/adapters/coc-api-adapter";
 
 jest.mock("axios");
 
@@ -23,4 +23,67 @@ test("verify should call post with correct parameters", async () => {
 test("verify should throw error when request fails", async () => {
   jest.mocked(axios.post).mockRejectedValue(new Error("Failed"));
   await expect(verify("#player", "token")).rejects.toThrow(new Error("Failed"));
+});
+
+test("getPlayer should call get with correct parameters", async () => {
+  jest.mocked(axios.get).mockResolvedValue({ data: {} });
+  await getPlayer("tag");
+  expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}/players/tag`, {
+    headers: {
+      Authorization: "Bearer API_TOKEN"
+    }
+  })
+});
+
+test("getPlayer should return player data", async () => {
+  jest.mocked(axios.get).mockResolvedValue({ data: { playertag: "tag" } });
+  const response = await getPlayer("tag");
+  expect(response).toEqual({ playertag: "tag" });
+});
+
+test('getPlayer should throw error when request fails', async () => {
+  jest.mocked(axios.get).mockRejectedValue(new Error("Failed"));
+  await expect(getPlayer("tag")).rejects.toThrow("Failed");
+});
+
+test("getClan should call get with correct parameters", async () => {
+  jest.mocked(axios.get).mockResolvedValue({ data: {} });
+  await getClan("tag");
+  expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}/clans/tag`, {
+    headers: {
+      Authorization: "Bearer API_TOKEN"
+    }
+  })
+});
+
+test("getClan should return clan data", async () => {
+  jest.mocked(axios.get).mockResolvedValue({ data: { clantag: "tag" } });
+  const response = await getClan("tag");
+  expect(response).toEqual({ clantag: "tag" });
+});
+
+test('getClan should throw error when request fails', async () => {
+  jest.mocked(axios.get).mockRejectedValue(new Error("Failed"));
+  await expect(getClan("tag")).rejects.toThrow("Failed");
+});
+
+test("getCwl should call get with correct parameters", async () => {
+  jest.mocked(axios.get).mockResolvedValue({ data: {} });
+  await getCwl("tag");
+  expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}/players/tag`, {
+    headers: {
+      Authorization: "Bearer API_TOKEN"
+    }
+  })
+});
+
+test("getCwl should return player data", async () => {
+  jest.mocked(axios.get).mockResolvedValue({ data: { clantag: "tag" } });
+  const response = await getCwl("tag");
+  expect(response).toEqual({ clantag: "tag" });
+});
+
+test('getCwl should throw error when request fails', async () => {
+  jest.mocked(axios.get).mockRejectedValue(new Error("Failed"));
+  await expect(getCwl("tag")).rejects.toThrow("Failed");
 });
--- bot/test/authorizer/authorizer.test.ts
+++ bot/test/authorizer/authorizer.test.ts
@@ -4,20 +4,36 @@ import nacl from "tweetnacl";
 
 jest.mock("tweetnacl");
 
-process.env.DISCORD_PUBLIC_KEY = "PUBLIC_KEY";
-const mockEvent = {
-  headers: {
-    "x-signature-timestamp": "timestamp",
-    "x-signature-ed25519": "signature",
-  },
-  body: JSON.stringify({ test: "test" }),
-} as unknown as APIGatewayProxyEvent;
+let mockEvent: APIGatewayProxyEvent;
 
 beforeEach(() => {
+  process.env.DISCORD_PUBLIC_KEY = "PUBLIC_KEY";
   jest.mocked(nacl.sign.detached.verify).mockReturnValue(true);
+  mockEvent = {
+    headers: {
+      "x-signature-timestamp": "timestamp",
+      "x-signature-ed25519": "signature",
+    },
+    body: JSON.stringify({ test: "test" }),
+  } as unknown as APIGatewayProxyEvent;
 });
 
 afterEach(jest.resetAllMocks);
+
+test("should return false when signature is not present", () => {
+  delete mockEvent.headers["x-signature-ed25519"];
+  expect(authorizeRequest(mockEvent)).toBeFalsy();
+});
+
+test("should return false when timestamp is not present", async () => {
+  delete mockEvent.headers["x-signature-timestamp"];
+  expect(authorizeRequest(mockEvent)).toBeFalsy();
+});
+
+test("should return false when DISCORD_PUBLIC_KEY is not present", async () => {
+  delete process.env.DISCORD_PUBLIC_KEY;
+  expect(authorizeRequest(mockEvent)).toBeFalsy();
+});
 
 test("should call verify with correct parameters", () => {
   authorizeRequest(mockEvent);
@@ -34,10 +50,5 @@ test("should return true when verification is successful", () => {
 
 test("should return false when verification is unsuccessful", () => {
   jest.mocked(nacl.sign.detached.verify).mockReturnValue(false);
-  expect(authorizeRequest(mockEvent)).toBeFalsy();
-});
-
-test("should return false when necessary data is not present", () => {
-  delete mockEvent.headers["x-signature-ed25519"];
   expect(authorizeRequest(mockEvent)).toBeFalsy();
 });
