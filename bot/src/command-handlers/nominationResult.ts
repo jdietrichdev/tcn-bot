3f67fb2e438e61e3cf49746eb21239875ed23db5
benn299 <benn.anhalt@gmail.com>
2025-11-21 17:06:43 -0600

    Update nominationResult.ts

--- bot/src/command-handlers/nominationResult.ts
+++ bot/src/command-handlers/nominationResult.ts
@@ -1,66 +1,98 @@
-import { 
-  APIEmbed,
-  APIMessageComponentInteraction,
+import {
+  APIApplicationCommandInteractionDataRoleOption,
+  APIApplicationCommandInteractionDataStringOption,
+  APIChatInputApplicationCommandInteraction,
+  ComponentType,
 } from "discord-api-types/v10";
+import { getCommandOptionData } from "../util/interaction-util";
 import { dynamoDbClient } from "../clients/dynamodb-client";
-import { GetCommand } from "@aws-sdk/lib-dynamodb";
-import { updateResponse } from "../adapters/discord-adapter";
-import { isActorAdmin } from "./utils";
+import { GetCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
+import {
+  deleteResponse,
+  grantRole,
+  removeRole,
+  updateMessage,
+} from "../adapters/discord-adapter";
 import { getConfig } from "../util/serverConfig";
+import { BUTTONS } from "../component-handlers/buttons";
 import { Proposal } from "../util/interfaces";
+import { Rank } from "../util/enums";
 
-export const nominationResults = async (
-  interaction: APIMessageComponentInteraction
+export const handleNominationResult = async (
+  interaction: APIChatInputApplicationCommandInteraction
 ) => {
   try {
-    if (
-      await isActorAdmin(
-        interaction.guild_id!,
-        interaction.member!.user.id,
-        getConfig(interaction.guild_id!)
+    const config = getConfig(interaction.guild_id!);
+    const id =
+      getCommandOptionData<APIApplicationCommandInteractionDataStringOption>(
+        interaction,
+        "proposal"
+      ).value;
+    const result =
+      getCommandOptionData<APIApplicationCommandInteractionDataStringOption>(
+        interaction,
+        "result"
+      ).value;
+    const role =
+      getCommandOptionData<APIApplicationCommandInteractionDataRoleOption>(
+        interaction,
+        "role"
+      )?.value;
+
+    const proposals = (
+      await dynamoDbClient.send(
+        new GetCommand({
+          TableName: "BotTable",
+          Key: {
+            pk: interaction.guild_id!,
+            sk: "rank-proposals",
+          },
+        })
       )
-    ) {
-      const message = interaction.message.id;
-      const proposalData = (
-        await dynamoDbClient.send(
-          new GetCommand({
-            TableName: "BotTable",
-            Key: {
-              pk: interaction.guild_id!,
-              sk: "rank-proposals",
-            },
-          })
-        )
-      ).Item!;
+    ).Item!;
 
-      const proposal = proposalData.proposals.find(
-        (proposal: Proposal) => proposal.message === message
-      );
+    const proposalData: Proposal = proposals.proposals.find(
+      (proposal: Proposal) => proposal.message === id
+    );
+    proposalData.result = result;
 
-      const resultEmbed = createResultsEmbed(proposal);
-
-      await updateResponse(interaction.application_id, interaction.token, {
-        embeds: [resultEmbed],
-      });
-    } else {
-      await updateResponse(interaction.application_id, interaction.token, {
-        content: `Talk to admins if you'd like to see detailed results`,
-      });
+    if (result === "Approve" && role) {
+      if (proposalData.type === "Promotion") {
+        await grantRole(interaction.guild_id!, proposalData.userId, role);
+      } else {
+        await removeRole(interaction.guild_id!, proposalData.userId, role);
+      }
     }
-  } catch (err) {
-    console.error(`Failure displaying nomination results: ${err}`);
-    await updateResponse(interaction.application_id, interaction.token, {
-      content: "There was an issue displaying results, please try again",
-    });
-  }
-};
+    await removeRole(
+      interaction.guild_id!,
+      proposalData.userId,
+      proposalData.rank === Rank.ELDER
+        ? config.TRIAL_ELDER_ROLE
+        : config.TRIAL_LEAD_ROLE
+    );
 
-const createResultsEmbed = (proposal: Proposal) => {
-  return {
-    title: `Current Results for ${proposal.username}`,
-    description: proposal.reason,
-    fields: proposal.votes.map((vote) => {
-      return { name: vote.user, value: `${vote.type}: ${vote.reason}` };
-    }),
-  } as APIEmbed;
+     await updateMessage(
+            proposalData.rank === Rank.ELDER ? config.ELDER_PROPOSAL_CHANNEL : config.LEAD_PROPOSAL_CHANNEL,
+             proposalData.message, {
+      content: `Proposal ${result === "Approve" ? "Approved" : "Denied"}`,
+      components: [
+        {
+          type: ComponentType.ActionRow,
+          components: [BUTTONS.NOMINATION_RESULTS],
+        },
+      ],
+    });
+
+    await dynamoDbClient.send(
+      new PutCommand({
+        TableName: "BotTable",
+        Item: proposals,
+      })
+    );
+
+    await deleteResponse(interaction.application_id, interaction.token);
+  } catch (err) {
+    console.log("Failure handling nomination result", err);
+    throw err;
+  }
 };
